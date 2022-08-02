9cf62097ba3390cd2e324154d3aebc602ebe6dab
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-08-02 14:13:56 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const text = "Sam is good at codingschool";
+
+//Aufgabe1
+let sam = text.replace("good", "bad");
+let sam1= sam.replace("codingschool", "School");
+console.log(sam)
+document.write(sam1 + "<br>")
+
+//Aufgabe 2
+let susi = text.replace("Sam", "Susi")
+let susi1 = susi.replace("codingschool", "School")
+document.write(susi1 + "<br>")
+console.log(susi)
+
+//Aufgabe 3
+let SamA = text.replace("codingschool", "Tennis")
+document.write(SamA +"<br>")
